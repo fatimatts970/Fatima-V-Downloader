b8f462bf13f337352e933f6ef3af9b3d776771c2
Fatima Mirza <Anjaanshahzadi27@gmail.com>
2026-07-17 18:50:03 +0500

    Add download route

--- api/index.py
+++ api/index.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 import os
 
-# Absolute path set karna taaki templates mil jayein
 template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../templates'))
 app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form.get('url')
+    return f"Aapne ye link bheji hai: {url}"
 
 if __name__ == '__main__':
     app.run(debug=True)
